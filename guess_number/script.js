ddcbf280ba88d1670af7ad82a7991c3cd7164ed5
Thiruppathi <thiruppathi@jnanain.in>
2024-11-26 11:27:28 +0530

    implementing dom manipulation logic

--- guess_number/script.js
+++ guess_number/script.js
@@ -1,1 +1,45 @@
 'use strict';
+let secretNumber = Math.trunc(Math.random() * 20) + 1;
+let score = 20;
+let highScore = 0;
+const number = document.querySelector('.number');
+const scoreDisplay = document.querySelector('.score');
+const highScoreDisplay = document.querySelector('.highscore');
+const guessInput = document.querySelector('.guess');
+const checkButton = document.querySelector('.check');
+const againButton = document.querySelector('.again');
+
+function displayMessage(message) {
+  document.querySelector('.message').textContent = message;
+}
+
+checkButton.addEventListener('click', function () {
+  const guess = Number(guessInput.value);
+  if (!guess) {
+    displayMessage('No number!');
+  } else if (guess === secretNumber) {
+    displayMessage('Correct number!');
+    number.textContent = secretNumber;
+    document.querySelector('body').style.backgroundColor = '#60b347';
+    number.style.width = '30rem';
+    if (score > highScore) {
+      highScore = score;
+      highScoreDisplay.textContent = highScore;
+    }
+  } else if (guess !== secretNumber) {
+    displayMessage(guess > secretNumber ? 'Too high!' : 'Too low!');
+    score--;
+    scoreDisplay.textContent = score;
+  }
+});
+
+againButton.addEventListener('click', function () {
+  score = 20;
+  secretNumber = Math.trunc(Math.random() * 20) + 1;
+  displayMessage('Start guessing...');
+  scoreDisplay.textContent = score;
+  number.textContent = '?';
+  guessInput.value = '';
+  document.querySelector('body').style.backgroundColor = '#222';
+  number.style.width = '15rem';
+});
